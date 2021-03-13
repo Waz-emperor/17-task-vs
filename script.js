1afa93644af3ee49d02a5fb6ea80cf2930af6488
Waz-emperor <64282899+Waz-emperor@users.noreply.github.com>
2021-03-13 19:09:47 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,38 @@
+
+
+function drawTree (hight) {
+    for (let i = 1; i <= hight; i++) {
+        let result = "";
+        for (let  f= i;  f< hight; f++) {
+            result = result + " "
+          
+        }
+      for (let j = 0; j < (1*i-1); j++) {
+        
+        result = result + "+ "
+      }
+
+      console.log(result)
+    }
+          
+          
+  } 
+
+  var levels = prompt('How many levels high should be the tree?');
+ 
+
+
+  //drawTree(levels);
+
+  if(levels == 0) {
+      alert("nope 0, try another")
+  }
+  else if(levels == 1) {
+      alert("nope 1, try bigger")
+  }
+  else {
+      console.log("ok, let's do this")
+    drawTree(levels);
+  }
+  
+ 
